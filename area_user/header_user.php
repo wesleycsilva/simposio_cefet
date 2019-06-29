3f830804d54327f7ed1538f3af7c1250c99f71c6
Wesley <wesleycsilva@yahoo.com.br>
2019-06-28 23:51:08 -0300

    menu dinamico

--- area_user/header_user.php
+++ area_user/header_user.php
@@ -1,3 +1,16 @@
+<?php require_once 'global_user.php' ?>
+<?php
+try {
+
+//    $idSimposista = $_GET['idSimposista'];
+    $idSimposista = 5;
+    $simposista = new SimposistaUser($idSimposista);
+    $mask = new FuncaoUser();
+
+} catch (Exception $e) {
+    Erro::trataErro($e);
+}
+?>
 <!-- HEADER DESKTOP-->
 <header class="header-desktop">
     <div class="section__content section__content--p30">
@@ -6,24 +19,21 @@
                 <div class="header-button">
                     <div class="account-wrap">
                         <div class="account-item clearfix js-item-menu">
-                            <!--                                        <div class="image">-->
-                            <!--                                            <img src="images/icon/avatar-01.jpg" alt="John Doe" />-->
-                            <!--                                        </div>-->
                             <div class="content">
-                                <a class="js-acc-btn" href="#">Wesley Caetano da Silva</a>
+                                <a class="js-acc-btn" href="#"><?= $simposista->nome?></a>
                             </div>
                             <div class="account-dropdown js-dropdown">
                                 <div class="info clearfix">
                                     <div class="image">
                                         <a href="#">
-                                            <img src="images/icon/avatar-01.jpg" alt="John Doe"/>
+                                            <img src="../img/simposio_logo.png" alt="John Doe"/>
                                         </a>
                                     </div>
                                     <div class="content">
                                         <h5 class="name">
-                                            <a href="#">Wesley Caetano da Silva</a>
+                                            <a href="#"><?= $simposista->nome?></a>
                                         </h5>
-                                        <span class="email">wesleycsilva@yahoo.com.br</span>
+                                        <span class="email"><?= $simposista->email?></span>
                                     </div>
                                 </div>
                                 <div class="account-dropdown__body">
