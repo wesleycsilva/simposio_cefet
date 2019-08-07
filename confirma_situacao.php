15849ad173de5f28145e3d5f1fe43708f948e061
wesleytiburcio <wesleytsantos@gmail.com>
2019-08-07 16:38:43 -0300

    att2

--- confirma_situacao.php
+++ confirma_situacao.php
@@ -11,27 +11,39 @@ try
 	$situacao = $_GET['situacao'];
 	// 1 = Cadastrado 2 = Cancelado 3 = Presente 4 = Ausente 5 = Inativo
 
-	if ($idSituacaoQr == 1)
-	{
-	    $inscricao->idEvento = $idEvento;
-	    $inscricao->idSimposista = $idSimposista;
-	    $inscricao->situacao = $situacao;
-	    $inscricao->urlQrCode = null;
-		
-		$retorno = $inscricao->confirmaSituacao($idInscricao, $idEvento, $idSimposista, $situacao, $urlQrCode));
+	switch ($situacao) {
+		case '1':
+			$inscricao->idEvento = $idEvento;
+		    $inscricao->idSimposista = $idSimposista;
+		    $inscricao->situacao = 3;
+		    $inscricao->urlQrCode = null;
+			
+			$retorno = $inscricao->confirmaSituacao($idInscricao, $idEvento, $idSimposista, $situacao, $urlQrCode));
 
-		if ($retorno['status'] == 200) 
-		{
-        	echo $retorno['status'];
-    	} else 
-    	{
-    		echo "Erro na atualização da inscrição";
-    	}
-	} else
-	{
-		echo "Presença não pose sefr confirmada";
-	}
+			if ($retorno['status'] == 200) 
+			{
+	        	echo $retorno['status'];
+	    	} else 
+	    	{
+	    		echo "Erro na atualização da inscrição";
+	    	}
+			break;
 
+		case '2':
+			echo "Usuário CANCELADO";
+			break;
+		case '3':
+			echo "Usuário PRESENTE, não podendo ser mais cadastrado no sistema";
+			break;
+		case '4':
+			echo "Usuário AUSENTE";
+			break;
+		case '5':
+			echo "Usuário INATIVO";
+			break;
+
+	}	
+	
 } catch (Exception $e) {
     Erro::trataErro($e);
 }
